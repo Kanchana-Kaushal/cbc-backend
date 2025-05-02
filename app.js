7e722ea792c3b380ac1c50da6c0fac95b739add4
Kanchana K. <kanchanak.bandara@gmail.com>
2025-05-03 00:01:30 +0530

    added error middleware to handle errors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import orderRouter from "./routes/order.route.js";
 import productRouter from "./routes/products.route.js";
+import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
 app.use(express.json());
@@ -15,12 +16,16 @@ app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 
+//This middleware will handle invalid requests
 app.use((req, res) => {
     res.status(404).json({
         success: false,
         message: "Resource not found",
     });
 });
+
+//This middleware will work if a error happens
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
     console.log("Server is listening on PORT " + PORT);
